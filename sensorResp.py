683c6342e1c6be9cb0e516ebc9b251eb126a1432
Seiji R Takagi <LondonTokyo76950@users.noreply.github.com>
2018-09-21 20:17:51 -0400

    first draft writing sensor response

--- sensorResp.py
+++ sensorResp.py
@@ -1,1 +1,13 @@
-from gpio
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(8, GPIO.IN)
+
+while True:
+    time.sleep(0.25)
+    if GPIO.input(8):
+        print("Pin 8 is HIGH")
+    else:
+        print("Pin 8 is LOW")
