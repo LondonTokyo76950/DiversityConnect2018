5ee523ba0024d35e98aebc1d5e1867381ae4b94f
Seiji R Takagi <LondonTokyo76950@users.noreply.github.com>
2018-09-24 18:43:31 -0400

    added for trip counter

--- sensorResp.py
+++ sensorResp.py
@@ -6,9 +6,18 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(8, GPIO.IN)
 
+tripCounter = 0
+
 while True:
-    time.sleep(0.25)
+    time.sleep(0.015)
     if GPIO.input(8):
         print("Pin 8 is HIGH")
+        if tripCounter == 0 or tripCounter == 5 or tripCounter == 10:
+            print("Alert!")
+        elif tripCounter >= 15:
+            print("Error!")
+        tripCounter += 1
+        time.sleep(1)
     else:
+        tripCounter = 0
         print("Pin 8 is LOW")
