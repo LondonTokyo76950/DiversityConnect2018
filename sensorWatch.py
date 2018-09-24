0f274dd80dbd17cb50b09036dbeb44b8cd395ab1
Seiji R Takagi <LondonTokyo76950@users.noreply.github.com>
2018-09-24 18:59:43 -0400

    added file for just watching sensor

---
+++ sensorWatch.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(8, GPIO.IN)
+
+tripCounter = 0
+
+while True:
+    time.sleep(0.015)
+    if GPIO.input(8):
+        print("Pin 8 is HIGH")
+        if tripCounter == 0 or tripCounter == 5 or tripCounter == 10:
+            print("Alert!")
+        elif tripCounter >= 15:
+            print("Error!")
+        print("Taking Photo")
+        # subprocess.call("./takePhoto.sh", shell=True)
+        tripCounter += 1
+        time.sleep(1)
+    else:
+        tripCounter = 0
+        print("Pin 8 is LOW")
